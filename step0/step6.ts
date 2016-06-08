efbc21e336a93f1588e8676cc278b58a9f55ccc8
sunlight999 <louis.sunlight999@gmail.com>
2016-06-08 06:27:50 +0000

    ts with class inheritance

---
+++ step0/step6.ts
@@ -0,0 +1,23 @@
+class Student {
+    fullName: string;
+    constructor(public firstName, public middleInitial, public lastName) {
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+}
+
+class ActivStudent extends Student {
+    constructor(public firstName, public middleInitial, public lastName,public mainCourse) {
+        super(firstName, middleInitial,lastName);
+   
+    }
+}
+
+
+function greeterActiv(student : ActivStudent) {
+    return "Hello, " + student.fullName +", student interested in   " + student.mainCourse;
+}
+
+
+let user = new ActivStudent("Jane", "M.", "User","Angular");
+
+document.body.innerHTML = greeterActiv(user);
